2bfa2dfb95befaa70c3c7ac5660509465651f4a5
cfd2000 <52366440+cfd2000@users.noreply.github.com>
2020-07-02 11:34:12 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+/*Optional*/
+/*Boleh Diganti, Boleh Tidak*/
+
+$boost_url = "av=1.0.0.122&v=4.0.8&bid=4b9d6f34-3873-4d07-ace7-fa3fdc9d7685&app_id=pro.cryptotab.android";
+$start = "bid=4b9d6f34-3873-4d07-ace7-fa3fdc9d7685&v=4.0.8";
+$versi = "av=1.0.0.84&v=4.0.8&bid=4b9d6f34-3873-4d07-ace7-fa3fdc9d7685";
+$data_boost = '{"boost_ratio":10}';
+
+/*Change According to Your Auth Data*/
+/*Ubah Sesuai Data Auth Kalian*/
+
+$auth[] = "gAAAAABe_Ud4upxpk_kGkqKM_m13nusIw4wWCFJhPRMdTZtMTTm1TqoZfciL_RPC6UMrGMpt9zc_JJMTdphmBYrl43NS9bnQHQ==
+user-agent: Mozilla/5.0 (Linux; Android 8.1.0; Redmi 5A) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.73 Mobile Safari/537.36";
+$auth[] = "gAAAAABe_Ud4upxpk_kGkqKM_m13nusIw4wWCFJhPRMdTZtMTTm1TqoZfciL_RPC6UMrGMpt9zc_JJMTdphmBYrl43NS9bnQHQ==";
+$user-agent = "Mozilla/5.0 (Linux; Android 8.1.0; Redmi 5A) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.73 Mobile Safari/537.36";
